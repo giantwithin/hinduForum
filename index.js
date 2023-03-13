e529501726be0aa76054e23b339fda7ee9522481
giantwithin <127123210+giantwithin@users.noreply.github.com>
2023-03-13 05:43:11 +0000

    links, hamburger menu

--- index.js
+++ index.js
@@ -35,3 +35,10 @@ function scroll(idS){
       });
     return false;
 }
+
+const button = document.getElementById("my-button");
+const nav = document.getElementById("mynav");
+
+button.addEventListener('click', () => {
+  nav.classList.toggle('show');
+});
